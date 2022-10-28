71e8390b9eeea811b5d590c3d79f618851f2d00f
ElmarAugustineFernandez <elmaraugustine.fernandez@students.isatu.edu.ph>
2022-10-28 23:02:58 +0800

    first commit

---
+++ Elmar_repo/ASSIGNMENT IRIS/Fernandez_ASSIGNMENT  versicolor.R
@@ -0,0 +1,96 @@
+ELMAR AUGUSTINE FERNANDEZ BSIT 2-A
+ACTIVIY
+
+
+data("iris")
+data
+
+subset(iris, Species == "versicolor")[1:50,]
+
+
+Sepal.Length Sepal.Width Petal.Length Petal.Width    Species
+51           7.0         3.2          4.7         1.4 versicolor
+52           6.4         3.2          4.5         1.5 versicolor
+53           6.9         3.1          4.9         1.5 versicolor
+54           5.5         2.3          4.0         1.3 versicolor
+55           6.5         2.8          4.6         1.5 versicolor
+56           5.7         2.8          4.5         1.3 versicolor
+57           6.3         3.3          4.7         1.6 versicolor
+58           4.9         2.4          3.3         1.0 versicolor
+59           6.6         2.9          4.6         1.3 versicolor
+60           5.2         2.7          3.9         1.4 versicolor
+61           5.0         2.0          3.5         1.0 versicolor
+62           5.9         3.0          4.2         1.5 versicolor
+63           6.0         2.2          4.0         1.0 versicolor
+64           6.1         2.9          4.7         1.4 versicolor
+65           5.6         2.9          3.6         1.3 versicolor
+66           6.7         3.1          4.4         1.4 versicolor
+67           5.6         3.0          4.5         1.5 versicolor
+68           5.8         2.7          4.1         1.0 versicolor
+69           6.2         2.2          4.5         1.5 versicolor
+70           5.6         2.5          3.9         1.1 versicolor
+71           5.9         3.2          4.8         1.8 versicolor
+72           6.1         2.8          4.0         1.3 versicolor
+73           6.3         2.5          4.9         1.5 versicolor
+74           6.1         2.8          4.7         1.2 versicolor
+75           6.4         2.9          4.3         1.3 versicolor
+76           6.6         3.0          4.4         1.4 versicolor
+77           6.8         2.8          4.8         1.4 versicolor
+78           6.7         3.0          5.0         1.7 versicolor
+79           6.0         2.9          4.5         1.5 versicolor
+80           5.7         2.6          3.5         1.0 versicolor
+81           5.5         2.4          3.8         1.1 versicolor
+82           5.5         2.4          3.7         1.0 versicolor
+83           5.8         2.7          3.9         1.2 versicolor
+84           6.0         2.7          5.1         1.6 versicolor
+85           5.4         3.0          4.5         1.5 versicolor
+86           6.0         3.4          4.5         1.6 versicolor
+87           6.7         3.1          4.7         1.5 versicolor
+88           6.3         2.3          4.4         1.3 versicolor
+89           5.6         3.0          4.1         1.3 versicolor
+90           5.5         2.5          4.0         1.3 versicolor
+91           5.5         2.6          4.4         1.2 versicolor
+92           6.1         3.0          4.6         1.4 versicolor
+93           5.8         2.6          4.0         1.2 versicolor
+94           5.0         2.3          3.3         1.0 versicolor
+95           5.6         2.7          4.2         1.3 versicolor
+96           5.7         3.0          4.2         1.2 versicolor
+97           5.7         2.9          4.2         1.3 versicolor
+98           6.2         2.9          4.3         1.3 versicolor
+99           5.1         2.5          3.0         1.1 versicolor
+100          5.7         2.8          4.1         1.3 versicolor
+
+
+
+calc_mean <- function(data = iris,  species = "versicolor", col = "Species"){
+  col       <- which(col %in% colnames(data))
+  return(mean(subset(data, Species == species)[, col]))
+}  
+calc_mean(iris, species = "versicolor", col = "Species")
+
+[1] 5.936
+
+
+
+tapply(iris$Sepal.Length, iris$Species, mean)
+
+setosa versicolor  virginica 
+5.006      5.936      6.588
+
+tapply(iris$Sepal.Width, iris$Species, mean)
+
+setosa versicolor  virginica 
+3.428      2.770      2.974 
+
+tapply(iris$Petal.Length, iris$Species, mean)
+
+setosa versicolor  virginica 
+1.462      4.260      5.552 
+
+tapply(iris$Petal.Width, iris$Species, mean)
+
+setosa versicolor  virginica 
+0.246      1.326      2.026 
+
+
+
